8599f016d0b764aba3b8dcf34bbd09ae35d712fa
MBPW <mbpw@MBPW.local>
2018-04-29 20:06:13 +0200

    :sparkles: carousel

--- src/components/ImageCarousel/ImageCarousel.js
+++ src/components/ImageCarousel/ImageCarousel.js
@@ -3,12 +3,39 @@ import { Link } from "react-static";
 import styled from "styled-components";
 import Carousel from "nuka-carousel";
 
+const Wrap = styled.section`
+  background-color: ${props => props.theme.palette.white};
+`;
+
+const StyledCarousel = styled(Carousel)`
+  max-width: ${props => props.theme.grid.maxWidth};
+  width: ${props => props.theme.grid.container};
+  margin: 0 auto;
+  margin-bottom: -8em;
+`;
+
 const ImageCarousel = ({ carouselImages }) => (
-  <Carousel>
-    {carouselImages.map(image => (
-      <img key={image.url} src={image.url} alt="carouselImage" />
-    ))}
-  </Carousel>
+  <Wrap>
+    <StyledCarousel
+      autoplay
+      wrapAround
+      // renderCenterLeftControls={({ previousSlide }) => (
+      //   <span onClick={previousSlide}>prev</span>
+      // )}
+      // renderCenterRightControls={({ nextSlide }) => (
+      //   <span onClick={nextSlide}>next</span>
+      // )}
+      // renderBottomCenterControls={({ slideCount, currentSlide, goToSlide }) => (
+      //   <span onClick={goToSlide}>
+      //     {currentSlide} / {slideCount}
+      //   </span>
+      // )}
+    >
+      {carouselImages.map(image => (
+        <img key={image.url} src={image.url} alt="carousel image" />
+      ))}
+    </StyledCarousel>
+  </Wrap>
 );
 
 export default ImageCarousel;
