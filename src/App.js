7dd36ff484dee609da7e9d57ad27f4c875048217
Wouter Landuydt <wouter.landuydt@gmail.com>
2018-08-16 19:41:40 +0200

    :fire: removed animstyles

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Fragment } from "react";
 import { Router } from "react-static";
 import { ThemeProvider, injectGlobal } from "styled-components";
 import branding, { globalStyles } from "./config/branding";
-import { countUpStyling } from "./config/animations";
 import reset from "styled-reset";
 import { hot } from "react-hot-loader";
 import Routes from "react-static-routes";
@@ -10,7 +9,6 @@ import Routes from "react-static-routes";
 injectGlobal`
   ${reset}
   ${globalStyles}
-  ${countUpStyling}
 `;
 
 const App = () => (
--- src/config/animations.js
+++ src/config/animations.js
@@ -1,4 +1,4 @@
-import { keyframes, css } from "styled-components";
+import { keyframes } from "styled-components";
 
 export const fadeIn = keyframes`
   from {
@@ -25,48 +25,5 @@ export const logoAnim = keyframes`
 
   100% {
     transform: scale(1);
-  }
-`;
-
-export const countUpStyling = css`
-  * {
-    margin: 0;
-    padding: 0;
-  }
-  ul,
-  ol,
-  li {
-    list-style: none;
-  }
-  .title {
-    text-align: center;
-  }
-  .exam-div {
-    padding: 20px 0;
-    text-align: center;
-    color: cornflowerblue;
-    font-size: 30px;
-  }
-  .restart-btn {
-    margin-top: 50px;
-    background-color: chocolate;
-    color: #ffffff;
-    padding: 10px 20px;
-    border-radius: 5px;
-  }
-  .count-roll-main,
-  .count-slide-main {
-    display: inline-block;
-    overflow: hidden;
-  }
-  .count-roll-ul {
-    overflow: hidden;
-    transition-timing-function: ease-out;
-    -moz-transition-timing-function: ease-out; /* Firefox 4 */
-    -webkit-transition-timing-function: ease-out; /* Safari 和 Chrome */
-    -o-transition-timing-function: ease-out; /* Opera */
-  }
-  .count-roll-li {
-    display: block;
   }
 `;
